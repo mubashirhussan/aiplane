c58a254b9e66dfa0ddc15aebfd8d2fde00640c8f
mubashirhussan <mubashirhussan93@gmail.com>
2025-04-25 16:14:54 +0500

    globe rotating updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="flex justify-end p-4 gap-6 ">
+    <header className="flex justify-end p-4 gap-6 z-10">
       <Link href="/about" className="hover:text-cyan-400">
         About
       </Link>
